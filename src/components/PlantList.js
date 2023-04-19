08532d81fbfe4b427b234779e69b37da0c52fc8e
Nicholas <crazedglitch@yahoo.com>
2023-04-19 17:57:51 -0500

    1st deliverable complete

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,10 +1,24 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({ plantListings }) {
+  console.log(plantListings)
+
+  const renderPlantCard = plantListings.map( plant =>
+      <PlantCard 
+        key={ plant.id }
+        plant={ plant }
+      />
+  )
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">
+      {/* render PlantCards components in here */}
+      { renderPlantCard }
+      
+    </ul>
   );
 }
 
 export default PlantList;
+
+// { renderPlantListings }
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,16 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
+import PlantCard from "./PlantCard";
+
+const baseUrl = "http://localhost:6001";
+const plantsUrl = baseUrl + "/plants"
 
 function PlantPage() {
+
+  const [ plantListings, setPlantListings ] = useState([])
+   useEffect( fetchPlants, [] )
+
+  function fetchPlants() {
+    fetch( plantsUrl )
+    .then( res => res.json())
+    .then( setPlantListings )
+  }
+
+
   return (
     <main>
       <NewPlantForm />
       <Search />
-      <PlantList />
+      <PlantList 
+        plantListings={ plantListings } 
+      />
     </main>
   );
 }
 
 export default PlantPage;
+
+
+
+//renderPlantListings={ renderPlantListings }
+
+// const singlePlant = plantListings.map( singlePlant => 
+  //     <PlantCard 
+  //       key={ singlePlant.id }
+  //       name={ singlePlant.name }
+  //       image={ singlePlant.image }
+  //       price={ singlePlant.price }
+  //     />
+  //   )
